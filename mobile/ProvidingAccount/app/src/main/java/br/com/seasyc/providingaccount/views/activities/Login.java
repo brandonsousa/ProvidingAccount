d04a62faabee0a6b9251581a4c70a186e683857f
Brandon Sousa da Silva <brdnsousa@gmail.com>
2020-08-09 18:19:32 -0300

    add api and managment

---
+++ mobile/ProvidingAccount/app/src/main/java/br/com/seasyc/providingaccount/views/activities/Login.java
@@ -0,0 +1,16 @@
+package br.com.seasyc.providingaccount.views.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.seasyc.providingaccount.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
