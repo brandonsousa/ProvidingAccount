f1ce5112d21430f62f2dfc85c55f5678835ac047
Brandon Sousa da Silva <brdnsousa@gmail.com>
2020-08-09 22:47:49 -0300

    add dashboard index card

--- managment/app/Controllers/Http/DashboardController.js
+++ managment/app/Controllers/Http/DashboardController.js
@@ -1,8 +1,21 @@
 'use strict'
 
+const Database = use('Database')
+
 class DashboardController {
-    async index({ request, view, response, auth }){
-        return view.render('dashboard.index')
+    async index({ view, auth }) {
+        
+        const food = await Database.raw('SELECT SUM(price) AS total FROM receipts WHERE category = "Food"')
+        const accommodation = await Database.raw('SELECT SUM(price) AS total FROM receipts WHERE category = "Accommodation"')
+        const transport = await Database.raw('SELECT SUM(price) AS total FROM receipts WHERE category = "Transport"')
+        const others = await Database.raw('SELECT SUM(price) AS total FROM receipts WHERE category = "Others"')
+        
+        return view.render('dashboard.index', {
+            food: food[0],
+            accommodation: accommodation[0],
+            transport: transport[0],
+            others: others[0],
+        })
     }
 }
 
