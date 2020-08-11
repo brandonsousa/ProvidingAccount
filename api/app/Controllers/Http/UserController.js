b3047d8177f657deb6b16b9ce80210815ac2cff1
Brandon Sousa da Silva <brdnsousa@gmail.com>
2020-08-11 17:53:22 -0300

    add index on usercontroller to return user data

--- api/app/Controllers/Http/UserController.js
+++ api/app/Controllers/Http/UserController.js
@@ -1,6 +1,14 @@
 'use strict'
 
+const User = use('App/Models/User')
 class UserController {
+    async index({ auth }) {
+        const user = await User.find(auth.user.id)
+        if (user) {
+            return response.status(200).send(user)
+        }
+        return response.status(404).send('error, user not found')
+    }
 }
 
 module.exports = UserController
