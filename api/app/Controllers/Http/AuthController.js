6e262d7b959a047b1d65eb2fe6c77e401a1bb271
Brandon Sousa da Silva <brdnsousa@gmail.com>
2020-08-11 17:50:13 -0300

    add controller; done auth controller

---
+++ api/app/Controllers/Http/AuthController.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const User = use('App/Models/User')
+
+class AuthController {
+    async login({ request, response, auth }) {
+        const { email, password } = request.all()
+
+        const token = await auth.attempt(email, password)
+
+        if (token) {
+            return response.status(200).send({
+                login: true,
+                token: token.token
+            })
+        }
+
+        return response.status(401).send({
+            login: false,
+            token: 'Invalid credentials, try again'
+        })
+    }
+
+    async logout({ response, auth }) {
+        await auth.logout()
+        return response.status(200).send({
+            logout: true
+        })
+    }
+}
+
+module.exports = AuthController
