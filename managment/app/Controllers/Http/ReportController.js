0b077aa87dd697f3de5d8f282dbdcee91a308106
Brandon Sousa da Silva <brdnsousa@gmail.com>
2020-08-13 19:14:08 -0300

    fix error on show receipt user to print

--- managment/app/Controllers/Http/ReportController.js
+++ managment/app/Controllers/Http/ReportController.js
@@ -1,7 +1,6 @@
 'use strict'
 
-const Database = use('Database')
-
+const Receipt = use('App/Models/Receipt')
 const User = use('App/Models/User')
 class ReportController {
 
@@ -15,18 +14,14 @@ class ReportController {
         const user = await User.findBy('code', code)
 
         if (user) {
-            await user.load('receipts')
-            const food = await Database.raw('SELECT ROUND(SUM(price),2) AS total FROM receipts WHERE category = "Food" AND user_id = ?', user.id)
-            const accommodation = await Database.raw('SELECT ROUND(SUM(price),2) AS total FROM receipts WHERE category = "Accommodation" AND user_id = ?', user.id)
-            const transport = await Database.raw('SELECT ROUND(SUM(price),2) AS total FROM receipts WHERE category = "Transport" AND user_id = ?', user.id)
-            const others = await Database.raw('SELECT ROUND(SUM(price),2) AS total FROM receipts WHERE category = "Others"  AND user_id = ?', user.id)
-            return view.render('report.template', {
-                user: user.toJSON(),
-                food: food[0],
-                accommodation: accommodation[0],
-                transport: transport[0],
-                others: others[0]
-            })
+            const my_receipts = await Receipt.query().where('user_id', user.id).fetch()
+            if (my_receipts) {
+                return view.render('report.template', {
+                    receipts: my_receipts.toJSON()
+                })
+            }
+            session.flash({ error: 'No receipts to you' })
+            return response.redirect('/report')
         }
 
         session.flash({ error: 'User not found' })
