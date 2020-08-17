4b87f9842e7d648103fb1e329c261b088d094322
Brandon Sousa da Silva <brdnsousa@gmail.com>
2020-08-17 17:44:56 -0300

    add get all receipts by user

--- managment/app/Controllers/Http/ReceiptController.js
+++ managment/app/Controllers/Http/ReceiptController.js
@@ -35,12 +35,30 @@ class ReceiptController {
    */
   async show({ params, response, view, auth }) {
     if (auth.user.id == params.id) {
-      const receipts = await Receipt.query().where({user_id: auth.user.id}).fetch()
+      const receipts = await Receipt.query().where({ user_id: auth.user.id }).fetch()
       return view.render('receipt.my', {
         receipts: receipts.toJSON()
       })
     }
     return response.redirect('/receipts')
+  }
+
+  async getByUser({ request, response }) {
+    const { search } = request.all()
+    const receipts = await Database.raw('SELECT u.code, u.username, r.* ' +
+      'FROM users u ' +
+      'INNER JOIN receipts r ON r.user_id = u.id ' +
+      'WHERE u.code = ? OR u.email = ?', [search, search])
+
+    if (receipts[0].length == 0) {
+      return response.status(400).send({
+        data: 'No content to user'
+      })
+    }
+
+    return response.status(200).send({
+      data: receipts[0]
+    })
   }
 
 }
